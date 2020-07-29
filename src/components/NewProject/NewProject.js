06f3f2d0a0a57dd248ebabf33cc4adf70763ff54
Debanik <debanik96@gmail.com>
2020-07-29 11:57:18 +0530

    @arajguha | updated project name validation

--- src/components/NewProject/NewProject.js
+++ src/components/NewProject/NewProject.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react'
 import classes from './NewProject.module.css'
 import Button from '../UI/Button/Button'
 import axios from 'axios'
+import AuxHoc from '../hoc/AuxHoc/AuxHoc'
 
-//change to functional component
-//class based created for debugging
+//change to functional component or move it to containers folder later
 class NewProject extends Component{
     state = {
-        title: ''
+        title: '',
+        nameError: false
     }
 
     handleInputChange = (e) => {
@@ -16,9 +17,12 @@ class NewProject extends Component{
 
     submitHandler = (e) => {
         e.preventDefault()
-        if(!this.state.title || this.state.title === '')
-            alert('Enter valid Project name')
+        if(!this.state.title || this.state.title === ''){
+            //alert('Enter valid Project name')
+            this.setState({nameError: true})
+        }
         else{
+            this.setState({nameError: false})
             const projectName = this.state.title.trim()
             console.log(projectName)
             
@@ -32,25 +36,36 @@ class NewProject extends Component{
     }
 
     render(){
+
         return (
-            <div className={classes.NewProject}>
-                <h2>Create New Project</h2>
-                <form>
-                    <input 
-                        type="text" 
-                        id="projectTitle" 
-                        placeholder="Project Name"
-                        style={{margin: '20px'}}
-                        value={this.state.title}
-                        onChange={e => this.handleInputChange(e)}
-                    /> 
-                    <br/>
-                    <Button
-                        label="Create"
-                        clickHandler={e => this.submitHandler(e)} 
-                    />
-                </form>
-            </div>
+            <AuxHoc>
+                <div className={classes.NewProject}>
+                    <h2>Create New Project</h2>
+                    <form>
+                        <input 
+                            type="text" 
+                            id="projectTitle" 
+                            placeholder="Project Name"
+                            style={{margin: '20px'}}
+                            value={this.state.title}
+                            onChange={e => this.handleInputChange(e)}
+                        />
+                        <br/>
+                        <Button
+                            label="Create"
+                            clickHandler={e => this.submitHandler(e)} 
+                        />
+                        {this.state.nameError ? 
+                            <p style={{
+                                color: 'red', 
+                                fontWeight: 'bold'
+                            }}>
+                                Invalid Name
+                            </p> : null
+                        }
+                    </form>
+                </div>
+            </AuxHoc>
         )
     }
 }
