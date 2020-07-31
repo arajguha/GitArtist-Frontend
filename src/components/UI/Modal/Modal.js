bc575697b6c6b017b97975ad0bffdc0fd17a244a
Debanik <debanik96@gmail.com>
2020-07-31 14:24:26 +0530

    added framer-motion npm package

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,18 +1,21 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 import './Modal.css' 
 
 const modal = props => {
     const clickHandler = (e) => {
         if(e.target.classList.contains("Backdrop")){
-            console.log('backdrop clicked')
             props.modalCloseHandler('')
         }
     }
     return (
         <div className="Backdrop" onClick={clickHandler}>
-            <div className="Modal">
+            <motion.div 
+                className="Modal"
+                animate={{opacity: 1}}
+            >
                 {props.children}
-            </div>
+            </motion.div>
         </div>
     )
 }
